57f2999941a4b5ab019d6d34294ef8a5ce622ad3
Timon Back <timon.back+git@gmail.com>
2019-08-17 19:53:11 +0200

    Calc the 10th fib number

--- src/example.ts
+++ src/example.ts
@@ -1,43 +1,43 @@
 
 export const smolFib = `
-    let count = 10;
+let count = 10;
 
-    let fib = |n| {
-        if n < 1 { return 0; };
-        if n == 1 { return 1; };
-        fib(n - 1) + fib(n - 1);
-    };
+let fib = |n| {
+    if n < 1 { return 0; };
+    if n == 1 { return 1; };
+    fib(n - 1) + fib(n - 2);
+};
 
-    let result = fib(count);
-    print("The result is:");
-    print(result);
+let result = fib(count);
+print("The result is:");
+print(result);
 `;
 
-// Should eval to 512
+// Should eval to 55
 
 export const smolBasic = `
-    let a = 1;
-    let b = 2;
-    let add = |x, y| { x + y; };
-    let result = add(a, b);
-    print("The result is");
-    print(result);
+let a = 1;
+let b = 2;
+let add = |x, y| { x + y; };
+let result = add(a, b);
+print("The result is");
+print(result);
 `;
 
 export const smolEasyTest = `
-    let x = 1;
-    let y = 7;
+let x = 1;
+let y = 7;
 
-    let do_math = |x, y| {
-        let add = |a, b| { a + b };
+let do_math = |x, y| {
+    let add = |a, b| { a + b };
 
-        print("x + y:");
-        print(add(x, y));
+    print("x + y:");
+    print(add(x, y));
 
-        if y > 3 {
-            print("y is greater than 3");
-        }
-    };
+    if y > 3 {
+        print("y is greater than 3");
+    }
+};
 
-    do_math(x, y);
+do_math(x, y);
 `;

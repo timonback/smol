13d6c43818b04df21b3a94b0e5450a7797797a91
Timon Back <timon.back+git@gmail.com>
2019-08-17 20:09:19 +0200

    Write intermediate lexer, parser and interpreter data to disk
    Nice for debugging and understanding of the AST

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
+import fs from "fs";
+import crypto from "crypto";
+
 import { lexer } from "./lexer";
 import { smolBasic, smolFib, smolEasyTest } from "./example";
 import { parser } from "./parser";
 import { interpreter } from "./interpreter";
 
 export const evaluate = (smol: string) => {
+    const filename = './dist/' + crypto.createHash('md5').update(smol).digest('hex');
+    console.log(`Compile code to ${filename}`)
+
     const tokens = lexer(smol);
+    fs.writeFileSync(filename + ".tokens.txt", JSON.stringify(tokens, null, 2));
+
     const ast = parser(tokens);
-    interpreter(ast, {});
+    fs.writeFileSync(filename + ".ast.txt", JSON.stringify(ast, null, 2));
+
+    const res = interpreter(ast, {});
+    fs.writeFileSync(filename + ".res.txt", JSON.stringify(res, null, 2));
 };
 
-evaluate(smolEasyTest);
+evaluate(smolFib);
